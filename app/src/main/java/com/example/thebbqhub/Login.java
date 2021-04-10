4733996e5e8fa69c72a47fbd1f453de47544938a
Abhishek <abhishek.rathore@xaviers.edu.in>
2021-04-10 11:40:39 +0530

    ..lo

--- app/src/main/java/com/example/thebbqhub/Login.java
+++ app/src/main/java/com/example/thebbqhub/Login.java
@@ -3,7 +3,14 @@ package com.example.thebbqhub;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -12,6 +19,7 @@ public class Login extends AppCompatActivity {
     TabLayout tabLayout;
     ViewPager viewPager;
     LoginAdapter1 adapter;
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +27,16 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         tabLayout=findViewById(R.id.tab_layout);
         viewPager=findViewById(R.id.view_page);
-        adapter=new LoginAdapter1(getSupportFragmentManager());
+        txt=findViewById(R.id.textView);
 
+        adapter=new LoginAdapter1(getSupportFragmentManager());
         adapter.AddFragment(new login_tab(), "Login");
         adapter.AddFragment(new signup_tab(), "Sign up");
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
 
-    }
+}
+
+
 }
--- app/src/main/java/com/example/thebbqhub/login_tab.java
+++ app/src/main/java/com/example/thebbqhub/login_tab.java
@@ -1,5 +1,6 @@
 package com.example.thebbqhub;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,7 +20,7 @@ public class login_tab extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup root=(ViewGroup)inflater.inflate(R.layout.login_tab,container,false);
-
+        Login lg=new Login();
         mobile=root.findViewById(R.id.mobile);
         pass=root.findViewById(R.id.pass);
         btn=root.findViewById(R.id.logbtn);
@@ -36,7 +37,13 @@ public class login_tab extends Fragment {
         pass.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(500).start();
         btn.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(800).start();
 
-
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getActivity(),HomeActivity.class);
+                startActivity(i);
+            }
+        });
         return root;
     }
 }
