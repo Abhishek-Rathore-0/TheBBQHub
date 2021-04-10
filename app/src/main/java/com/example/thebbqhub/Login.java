a6f2fdc780757555b787acf70616f20bcfedc301
Crux <18acharyamalay@gmail.com>
2021-04-10 12:24:35 +0530

    color changed

--- app/src/main/java/com/example/thebbqhub/Login.java
+++ app/src/main/java/com/example/thebbqhub/Login.java
@@ -19,7 +19,7 @@ public class Login extends AppCompatActivity {
     TabLayout tabLayout;
     ViewPager viewPager;
     LoginAdapter1 adapter;
-    TextView txt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +27,7 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         tabLayout=findViewById(R.id.tab_layout);
         viewPager=findViewById(R.id.view_page);
-        txt=findViewById(R.id.textView);
+
 
         adapter=new LoginAdapter1(getSupportFragmentManager());
         adapter.AddFragment(new login_tab(), "Login");
