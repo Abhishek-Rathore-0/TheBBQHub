c1e1b5867f1a881e5a9e3e77bd2cd31706e8f57a
Abhishek <abhishek.rathore@xaviers.edu.in>
2021-04-12 18:46:22 +0530

    updated ..

--- app/src/main/java/com/example/thebbqhub/HomeFragment.java
+++ app/src/main/java/com/example/thebbqhub/HomeFragment.java
@@ -4,16 +4,65 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class HomeFragment extends Fragment {
+    TextView txt;
+    ProgressBar pg;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+
+    private String userId;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup root=(ViewGroup)inflater.inflate(R.layout.fragment_homee,container,false);
+        txt=(TextView)root.findViewById(R.id.welcome);
+        pg= root.findViewById(R.id.progress);
+        user= FirebaseAuth.getInstance().getCurrentUser();
+        reference= FirebaseDatabase.getInstance().getReference("Users");
+        userId=user.getUid();
 
-        return inflater.inflate(R.layout.fragment_homee,container,false);
+        reference.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userp=snapshot.getValue(User.class);
+
+                if(userp !=null){
+                    String name=userp.name;
+                    txt.setText("Welcome, "+name+" !");
+                    pg.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+        return root;
     }
 }
