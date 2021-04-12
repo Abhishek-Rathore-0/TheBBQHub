5beff7ae26ca3f666233251b24cd15ea8353f756
Abhishek <abhishek.rathore@xaviers.edu.in>
2021-04-12 17:58:27 +0530

    updated

--- app/src/main/java/com/example/thebbqhub/HomeActivity.java
+++ app/src/main/java/com/example/thebbqhub/HomeActivity.java
@@ -1,8 +1,15 @@
 package com.example.thebbqhub;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +17,38 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        BottomNavigationView bottomNav=findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment=null;
+
+
+            switch(item.getItemId())
+            {
+                case R.id.nav_home:
+                    selectedFragment=new HomeFragment();
+                    break;
+
+
+                case R.id.nav_contact:
+                    selectedFragment=new ContactFragment();
+                    break;
+
+                case R.id.nav_profile:
+                    selectedFragment=new ProfileFragment();
+                    break;
+
+
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,selectedFragment).commit();
+            return true;
+        }
+    };
 }
